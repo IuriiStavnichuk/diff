9060fd9d6b406ea43aeb1e10385403613cb6d1e3
iuriistavnichuk <info@fastforwardpro.com>
2014-10-23 23:43:17 -0400

    1

--- test/screenshot-diff.js
+++ test/screenshot-diff.js
@@ -4,19 +4,24 @@ var PNGDiff = require('png-diff');
 
 var urls=[
     "http://ya.ru",
-    "http://google.com",
-]
+    "http://google.com"
+];
 
-var sourceFolderName = 'screenshots-for-processing/source-versions/';
-var targetFolderName = 'screenshots-for-processing/target-versions/';
-var diffFolderName = 'screenshots-for-processing/diff-versions/';
+var sourceFolderName = 'source-versions/';
+var targetFolderName = 'target-versions/';
+
+var dt = new Date();
+var formattedTime=dt.getFullYear() + "." + (1+dt.getMonth()) + "." + dt.getDate()+ "-" + dt.getHours()+ "." + dt.getMinutes();
+var resultFolderName = 'results/'+formattedTime+'/';
+fs.mkdir( resultFolderName , 0777, true, function (err) { if (err) { console.log(err);} else { console.log('Directory created >>>'); } });
+
 
 for (var i in urls) {
 
     var screenshotName = urls[i].split('/').pop() + '.png';
 
     var image2Stream = fs.createReadStream(targetFolderName + screenshotName);
-    PNGDiff.outputDiff(sourceFolderName + screenshotName, image2Stream, diffFolderName + screenshotName, function (err, diffMetric ) {
+    PNGDiff.outputDiff(sourceFolderName + screenshotName, image2Stream, resultFolderName + screenshotName, function (err, diffMetric ) {
         if (err) throw err;
         console.log(diffMetric === 1 ? screenshotName + ': Difference detected.' : 'No difference');
     });
